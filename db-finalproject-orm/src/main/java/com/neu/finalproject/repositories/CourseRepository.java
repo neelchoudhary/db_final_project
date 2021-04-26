360da0bfc18c6e461f0034515e22ec5b0a5e7a8e
Neel Choudhary <neel.choudhary3@gmail.com>
2021-04-26 04:00:55 -0400

    removed useless stuff

--- db-finalproject-orm/src/main/java/com/neu/finalproject/repositories/CourseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.neu.finalproject.repositories;
-
-import com.neu.finalproject.models.Course;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CourseRepository
-        extends CrudRepository<Course, Integer> {
-}
--- db-finalproject-orm/src/main/java/com/neu/finalproject/repositories/SectionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.neu.finalproject.repositories;
-
-import com.neu.finalproject.models.Section;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SectionRepository
-        extends CrudRepository<Section, Integer> {
-}
